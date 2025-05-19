7fde5207e705b8a792f5e923895b7e24298c7393
Amilnishan <amilnishan7@gmail.com>
2025-05-19 15:08:08 +0530

    Program 10

---
+++ window_dir.sh
@@ -0,0 +1,14 @@
+#program to print directory name,date&time,size
+
+echo "Directory of $(pwd)"
+ls -lnA --time-style='+%m/%d/%Y %I:%M %p' | tail -n +2 | while read -r perm links owner group size date time filename
+do
+	firstchar=$(echo "$perm" | cut -c1)
+	if [ "$firstchar" = "d" ]
+	then
+		echo "$date $time     <DIR>     $filename"
+	elif [ "$frstchar" = "-" ]
+	then
+		echo "$date $time               $size $filename"
+	fi
+done
